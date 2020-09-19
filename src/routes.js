08260f0c0c32dd674ff37ec70c3a8350e4bc4ad1
Bruna Letícia Schneiders <bruna.schneiders@gmail.com>
2020-09-19 19:41:44 -0300

    ajuste na autenticação das rotas

--- src/routes.js
+++ src/routes.js
@@ -10,12 +10,10 @@ import authMiddleware from './app/middlewares/auth';
 const routes = Router();
 routes.use(cors());
 
-routes.get('/', (req, res) => res.json({ result: 'TEST-API' }));
+routes.get('/', (req, res) => res.json({ result: 'NOTEPAD API' }));
 
 // ROUTES PARA USER
 routes.post('/users', UserController.store);
-routes.get('/users', UserController.index);
-routes.get('/users/:uid', UserController.show);
 
 // // ROUTES PARA AUTH
 routes.post('/login', AuthController.store);
@@ -24,6 +22,8 @@ routes.use(authMiddleware);
 
 // ROUTES PARA USER(auth)
 routes.put('/users/:uid', UserController.update);
+routes.get('/users', UserController.index);
+routes.get('/users/:uid', UserController.show);
 
 // ROUTES PARA CARD(auth)
 routes.post('/cards', CardController.store);
